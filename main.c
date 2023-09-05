4a063ca3b1f8c7058c29a5d88b87100dd479dd71
dt0211 <141922680+dt0211@users.noreply.github.com>
2023-09-05 12:33:21 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,17 @@
 /*
  * main.c
- *
  * Created: 04-Sep-23 4:05:24 PM
  *  Author: Dinh Tuyen
  */ 
-
-
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void)
-{
-	DDRB = 0x01;
-	while(1)
-	{   PORTB = 0x01;
-		_delay_ms(1000);
-		PORTB = 0x00;
-		_delay_ms(1000);
-	}
+int main(void){
+  DDRB = 0x01;
+  while(1){
+    PORTB = 0x01;
+    _delay_ms(1000);
+    PORTB = 0x00;
+    _delay_ms(1000);
+  }
 }
